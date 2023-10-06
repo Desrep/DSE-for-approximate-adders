e60f57c5f59195b3c3a81671cd8360b41189e331
fep <ferpomu@gmail.com>
2023-10-05 21:50:05 -0600

    unconstrained moo, 2 variables, 3 objectives

--- eda_flow.py
+++ eda_flow.py
@@ -1,19 +1,14 @@
 ####Dummy flow, tests every adder in database by replacing it in the dut#############
 
 import numpy as np
-
-from pymoo.algorithms.soo.nonconvex.ga import GA
 from pymoo.core.problem import Problem
-from pymoo.operators.crossover.sbx import SBX
-from pymoo.operators.mutation.pm import PM
-from pymoo.operators.repair.rounding import RoundingRepair
-from pymoo.operators.sampling.rnd import IntegerRandomSampling
+from pymoo.visualization.scatter import Scatter
+from pymoo.algorithms.moo.nsga2 import RankAndCrowdingSurvival
+from pymoo.core.mixed import MixedVariableGA
 from pymoo.optimize import minimize
-
-
-
+from pymoo.core.problem import ElementwiseProblem
+from pymoo.core.variable import Real, Integer, Choice, Binary
 import os
-from scipy.optimize import basinhopping
 
 
 
@@ -23,34 +18,42 @@ with open('design_space.txt','w') as space:
 
 
 
-minimizer_kwargs = {"method": "BFGS"}
 
-class ThisProblem(Problem):
+class ThisProblem(ElementwiseProblem):
 
-    def __init__(self):
-        super().__init__(n_var=2, n_obj=1, n_ieq_constr = 1, xl = np.array([0,0]), xu = np.array([1,3]), vtype = int)
+    def __init__(self,**kwargs):
+        vars = {
+            "x": Integer(bounds=(0, 1)),
+            "y":Integer(bounds=(0,3))
+        }
+        super().__init__(vars=vars, n_obj=3,n_ieq_constr=0,**kwargs)
+
+
+
 
     def _evaluate(self,x,out,*args,**kwargs):
-        out["F"] = np.array(objective_func(x))
-        print(out["F"])
-        out["G"] = out["F"]-900 
+        x,y = x["x"],x["y"]
+        out["F"] = [objective_func(x,y)]
+        print(out)
+        #out["G"] = out["F"][0][0]+out["F"][0][1]*1e5+out["F"][0][2]-900 
 
-def objective_func(x):
-    print(x)
-    if((x[0,0]==0)and(x[0,1]==0)):
+def objective_func(x,y):
+    if((x==0)and(y==0)):
         atype = 'STD'
-    elif((x[0,0]==0)and(x[0,1]==1)):
+    elif((x==0)and(y==1)):
         atype = 'LOA1'
-    elif((x[0,0]==0)and(x[0,1]==2)):
+    elif((x==0)and(y==2)):
         atype = 'LOA2'
-    elif((x[0,0]==0)and(x[0,1]==3)):
+    elif((x==0)and(y==3)):
         atype = 'LOA3'
-    elif((x[0,0]==1)and(x[0,1]==1)):
+    elif((x==1)and(y==1)):
         atype = 'LOWA1'
-    elif((x[0,0]==1)and(x[0,1]==2)):
+    elif((x==1)and(y==2)):
         atype = 'LOWA2'
-    elif((x[0,0]==1)and(x[0,1]==3)):
+    elif((x==1)and(y==3)):
         atype = 'LOWA3'
+    else:
+        atype = 'STD'
     print(atype)
     with open('adder_selection.txt','w') as select:
         select.write(atype)
@@ -62,30 +65,23 @@ def objective_func(x):
             if(line == 1):
                 with open('design_space.txt','a') as space:
                     space.write(data+'\n')
-                f = [float(data.split()[1].strip(','))+float(data.split()[2].strip(','))*1e5+float(data.split()[3].strip(','))]
-                print(f)
+                f = [float(data.split()[1].strip(',')), float(data.split()[2].strip(','))*1e5, float(data.split()[3].strip(','))]
     return f
 
 problem = ThisProblem()
 
-method = GA(pop_size=1,
-            sampling=IntegerRandomSampling(),
-            crossover=SBX(prob=1.0, eta=3.0, vtype=float, repair=RoundingRepair()),
-            mutation=PM(prob=1.0, eta=3.0, vtype=float, repair=RoundingRepair()),
-            eliminate_duplicates=True,
-            )
+algorithm = MixedVariableGA(pop_size=10, survival=RankAndCrowdingSurvival())
 
 res = minimize(problem,
-               method,
-               termination=('n_gen', 15),
+               algorithm,
+               ('n_gen', 20),
                seed=1,
-               save_history=True
-               )
+               verbose=False)
 
-print("Best solution found: %s" % res.X)
-print("Function value: %s" % res.F)
-print("Constraint violation: %s" % res.CV)
-
-
+plot = Scatter()
+plot.add(problem.pareto_front(), plot_type="line", color="black", alpha=0.7)
+plot.add(res.F, facecolor="none", edgecolor="red")
+plot.show()
 
 
+print("Best solution found: \nX = %s\nF = %s" % (res.X, res.F))
